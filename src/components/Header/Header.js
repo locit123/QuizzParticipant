5e4af31af290f8242e265e9fe2df2ec04f6b3ec6
phung loc <phungloc6102003@gmail.com>
2024-07-09 17:41:26 +0700

    success react-router-dom

---
+++ src/components/Header/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">LocIt</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavLink className={"nav-link"} to="/">
+              Home
+            </NavLink>
+            <NavLink to="/user" className={"nav-link"}>
+              User
+            </NavLink>
+            <NavLink to="/admin" className={"nav-link"}>
+              Admin
+            </NavLink>
+          </Nav>
+          <Nav>
+            <NavDropdown title="Setting" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Logout</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
